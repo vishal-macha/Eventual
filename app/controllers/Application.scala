ab2c16f7404917ec4ae76f07dd1abf2931bf18b0
vishal-macha <vishalmacha5@gmail.com>
2017-04-09 19:57:46 -0700

    Adding models, views & controllers

---
+++ app/controllers/Application.scala
@@ -0,0 +1,34 @@
+package controllers
+
+import java.io.File
+
+import play.Play
+import play.api.mvc.Action
+import play.api.mvc.Controller
+
+/*
+ * Author: Vishal Macha
+ */
+object Application extends Controller {
+  /** serve the index page app/views/index.scala.html */
+  def index(any: String) = Action {
+    Ok(views.html.index())
+  }
+
+  /** resolve "any" into the corresponding HTML page URI */
+  def getURI(any: String): String = any match {
+    case "main" => "/public/html/main.html"
+    case "detail" => "/public/html/detail.html"
+    case _ => "error"
+  }
+
+  /** load an HTML page from public/html */
+  def loadPublicHTML(any: String) = Action {
+    val projectRoot = Play.application().path()
+    val file = new File(projectRoot + getURI(any))
+    if (file.exists())
+      Ok(scala.io.Source.fromFile(file.getCanonicalPath()).mkString).as("text/html");
+    else
+      NotFound
+  }
+}
